d0ae181c25de8f3284ad7b333b464cf347e9472e
Joel <s211192@student.dhbw-mannheim.de>
2023-07-04 23:18:13 +0200

    usermodel

---
+++ lib/models/User.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  final String id;
+  String? fullname;
+  String mail;
+  String role;
+
+  UserModel(
+      {required this.id,
+      required this.mail,
+      required this.role,
+      this.fullname});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      fullname: json['fullname'],
+      mail: json['mail'],
+      role: json['role'],
+    );
+  }
+
+  factory UserModel.fromSnapshot(DocumentSnapshot snapshot) {
+    Map data = snapshot.data() as Map<String, dynamic>;
+    return UserModel(id: data['id'], mail: data['mail'], role: data['role']);
+  }
+
+  Map<String, Object?> toJson() {
+    return {'id': id, 'fullname': fullname, 'mail': mail, 'role': role};
+  }
+}
